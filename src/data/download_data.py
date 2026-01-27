ad475af3e4ec6ad873460197426eb4e61bb64b2e
jonas-send <datascience@jonassend.com>
2026-01-27 10:07:20 +0100

    adjust to new version

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -4,6 +4,8 @@ import os
 from numerapi import NumerAPI
 from src.util.constants import DATA_PATH, PATH_RAW_TRAIN_SET, PATH_RAW_VALIDATE_SET, PATH_RAW_META_MODEL
 
+VERSION = "v5.2"
+
 
 def download_data() -> None:
     (DATA_PATH / 'raw').mkdir(parents=True, exist_ok=True)
@@ -16,8 +18,8 @@ def download_data() -> None:
     if os.path.exists(PATH_RAW_META_MODEL):
         os.remove(PATH_RAW_META_MODEL)
 
-    numer_api.download_dataset("v5.0/train.parquet", PATH_RAW_TRAIN_SET)
-    numer_api.download_dataset("v5.0/validation.parquet", PATH_RAW_VALIDATE_SET)
-    numer_api.download_dataset("v5.0/meta_model.parquet", PATH_RAW_META_MODEL)
+    numer_api.download_dataset(f"{VERSION}/train.parquet", PATH_RAW_TRAIN_SET)
+    numer_api.download_dataset(f"{VERSION}/validation.parquet", PATH_RAW_VALIDATE_SET)
+    numer_api.download_dataset(f"{VERSION}/meta_model.parquet", PATH_RAW_META_MODEL)
 
     print('Raw data downloaded.')
